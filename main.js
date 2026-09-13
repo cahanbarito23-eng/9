ffa93adb3ca0db0f2228ef705c7dea9ec6b0862e
cahanbarito23-eng <cahanbarito23@gmail.com>
2026-09-13 18:44:35 +0700

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function joinWhatsAppGroup() {
 }
 
 function openFacebookPage() {
-    window.open("https://www.facebook.com/share/g/1BJTvJ1xmx/");
+    window.open("https://www.facebook.com/share/g/1CXtzKgDbU/");
 }
 const video = document.getElementById("video");
 const overlay = document.getElementById("videoOverlay");
